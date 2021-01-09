618d1e00847af81040c5bc96a370e79abfed9026
Ali Mantaev <mantaev.vfl@gmail.com>
2021-01-09 12:35:25 -0500

    Initial Commit

---
+++ League/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  League
+//
+//  Created by Ali Mantaev on 2021-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ League/LeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeagueApp.swift
+//  League
+//
+//  Created by Ali Mantaev on 2021-01-09.
+//
+
+import SwiftUI
+
+@main
+struct LeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
